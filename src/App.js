741db23ee3e9ae5d856b237630333df86514a2e4
Fernando Martinez <fernando817mm@gmail.com>
2022-02-01 12:19:44 -0600

    Reducing prop drilling by moving functions to Student component

--- src/App.js
+++ src/App.js
@@ -5,34 +5,6 @@ import departments from "./json/departments.json";
 import Filter from "./json/components/Filter";
 import Student from "./json/components/Student";
 
-const formatDOB = (inputDate) => {
-  const date = new Date(inputDate).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
-
-  const dateSplit = date.split("/");
-  const correctDay = parseInt(dateSplit[1]) + 1;
-
-  return dateSplit[0] + "/" + correctDay + "/" + dateSplit[2];
-};
-
-const formatAdmitted = (inputDate) => {
-  return new Date(inputDate).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-};
-
-const formatGrad = (inputDate) => {
-  return new Date(inputDate).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "long",
-  });
-};
-
 const App = () => {
   const studentArr = students.body.studentData.students;
 
@@ -44,14 +16,7 @@ const App = () => {
           <div className="data-wrapper">
             <ul className="filtered-student">
               {studentArr.map((student) => {
-                return (
-                  <Student
-                    student={student}
-                    formatGrad={formatGrad}
-                    formatDOB={formatDOB}
-                    formatAdmitted={formatAdmitted}
-                  />
-                );
+                return <Student student={student} />;
               })}
             </ul>
           </div>
--- src/json/components/Student.js
+++ src/json/components/Student.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+const formatDOB = (inputDate) => {
+  const date = new Date(inputDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  const dateSplit = date.split("/");
+  const correctDay = parseInt(dateSplit[1]) + 1;
+
+  return dateSplit[0] + "/" + correctDay + "/" + dateSplit[2];
+};
+
+const formatAdmitted = (inputDate) => {
+  return new Date(inputDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+const formatGrad = (inputDate) => {
+  return new Date(inputDate).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+  });
+};
+
 const Student = (props) => {
-  const { student, formatAdmitted, formatDOB, formatGrad } = props;
+  const { student } = props;
   return (
     <li key={student.id}>
       <span className="name">
